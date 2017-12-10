57143918c530c629674405faa1c51b09ea70d0db
Jacob VanDomelen <jacob.vandome15@gmail.com>
2017-12-10 06:05:16 -0800

    Starting to add some more code.

---
+++ UnityProject/Bouncy Ball Racers/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public CharacterController Controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
