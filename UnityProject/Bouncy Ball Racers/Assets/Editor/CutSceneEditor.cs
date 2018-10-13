edb0ef9a57baf53c7968199309bbfbe53e270c69
Jacob VanDomelen <jacob.vandome15@gmail.com>
2018-10-13 02:30:30 -0700

    Added better positioning and better distinction for editor.

--- UnityProject/Bouncy Ball Racers/Assets/Editor/CutSceneEditor.cs
+++ UnityProject/Bouncy Ball Racers/Assets/Editor/CutSceneEditor.cs
@@ -30,21 +30,18 @@ public class CutSceneEditor : Editor
 
         // Displaying a field so that the user can specify the next scene
         EditorGUILayout.PropertyField(NextScene);
+        EditorGUILayout.Space();
 
         // Starting a loop for all of the panels currently on the object.
         for (int i = 0; i < myTarget.Panels.Count; i++)
         {
-            EditorGUILayout.Space();
-
-            if (GUILayout.Button("Remove Panel"))
-            {
-                myTarget.Panels.RemoveAt(i);
-                break;
-            }
-
             var panel = myTarget.Panels[i];
             SerializedObject obj = new SerializedObject(panel);
             obj.Update();
+
+            var panelArea = EditorGUILayout.BeginVertical();
+            GUI.Box(panelArea, "Panel");
+            GUILayout.Space(EditorGUIUtility.singleLineHeight * 2);
 
             EditorGUILayout.PropertyField(obj.FindProperty("CharacterName"));
             EditorGUILayout.PropertyField(obj.FindProperty("CharacterPortrait"));
@@ -62,44 +59,83 @@ public class CutSceneEditor : Editor
             EditorGUILayout.PropertyField(obj.FindProperty("Background"));
             EditorGUILayout.PropertyField(obj.FindProperty("SoundEffect"));
             EditorGUILayout.PropertyField(obj.FindProperty("Music"));
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+
+            GUILayout.Space(15);
+
+            EditorGUILayout.BeginVertical();
 
             // Starting a loop for all of the images on the current panel.
             for (int j = 0; j < panel.Images.Count; j++)
             {
-                EditorGUILayout.Space();
-
-                if (GUILayout.Button("Remove Image"))
-                {
-                    panel.Images.RemoveAt(j);
-                    break;
-                }
-
                 var image = panel.Images[j];
                 SerializedObject imageObject = new SerializedObject(image);
                 imageObject.Update();
+
+                EditorGUILayout.Space();
+                var imageArea = EditorGUILayout.BeginVertical();
+                GUI.Box(imageArea, "Image");
+                GUILayout.Space(EditorGUIUtility.singleLineHeight * 2);
 
                 EditorGUILayout.PropertyField(imageObject.FindProperty("Image"));
                 EditorGUILayout.PropertyField(imageObject.FindProperty("ZIndex"));
                 EditorGUILayout.PropertyField(imageObject.FindProperty("Position"));
                 EditorGUILayout.PropertyField(imageObject.FindProperty("Animation"));
 
+                if (GUILayout.Button("Remove Image"))
+                {
+                    panel.Images.RemoveAt(j);
+                    imageObject.ApplyModifiedProperties();
+                    break;
+                }
+
+                EditorGUILayout.Space();
+                EditorGUILayout.EndVertical();
+
                 imageObject.ApplyModifiedProperties();
             }
 
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.FlexibleSpace();
             if (GUILayout.Button("Add Image"))
             {
                 panel.Images.Add(ScriptableObject.CreateInstance<CutSceneImage>());
             }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+
+            GUILayout.Space(15);
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Remove Panel"))
+            {
+                myTarget.Panels.RemoveAt(i);
+                obj.ApplyModifiedProperties();
+                break;
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space();
 
             obj.ApplyModifiedProperties();
         }
 
-        EditorGUILayout.Space();
-
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        GUILayout.FlexibleSpace();
         if (GUILayout.Button("Add Panel"))
         {
             myTarget.Panels.Add(ScriptableObject.CreateInstance<CutScenePanel>());
         }
+        EditorGUILayout.EndHorizontal();
 
         // Applying all of the changed properties that took place during the function.
         serializedObject.ApplyModifiedProperties();
